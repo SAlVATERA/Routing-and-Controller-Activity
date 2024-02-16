c7ab9f97f812bf032b0e46eb484d4b1cfbc6714c
SAlVATERA <jasonsalvatera24680@gmail.com>
2024-02-16 16:43:29 +0800

    added a nav bar with 3 buttons named about-me,skills,and hobbies

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,13 @@
         </style>
     </head>
     <body class="antialiased">
+        <nav>
+            <ul>
+                <li><a href="{{ url('/about-me') }}">About Me</a></li>
+                <li><a href="{{ url('/skills') }}">Skills</a></li>
+                <li><a href="{{ url('/hobbies') }}">Hobbies</a></li>
+            </ul>
+        </nav>
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
             @if (Route::has('login'))
                 <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
